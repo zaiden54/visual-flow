ccc58dec300aa74ab60a636617be2fceef52437d
hegyth <hegyth1236@gmail.com>
2023-08-31 11:19:20 +0300

    auth

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,36 +1,24 @@
-import React, { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import elbrusLogo from './assets/elbrus.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import { Container } from '@mui/material';
+import { Route, Routes } from 'react-router-dom';
+import { useAppDispatch } from './redux/hooks/reduxHooks';
+import { checkUserThunk } from './redux/slices/user/userThunks';
+import AuthPage from './components/pages/AuthPage';
 
 function App(): JSX.Element {
-  const [count, setCount] = useState(0);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    void dispatch(checkUserThunk());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://github.com/Elbrus-Bootcamp" target="_blank" rel="noreferrer">
-          <img src={elbrusLogo} className="logo elbrus" alt="Elbrus logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h2>Elbrus Bootcamp</h2>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button type="button" onClick={() => setCount((prev) => prev + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </div>
+    <Container>
+    <Routes>
+      <Route path="/auth/:auth" element={<AuthPage />} />
+    </Routes>
+  </Container>
   );
 }
 
