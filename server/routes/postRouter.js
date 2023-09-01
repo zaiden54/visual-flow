953db49ca576d05213e40d5e4821821526f475e1
Denis Zaynagabdinov <zaiden.ranepa@gmail.com>
2023-09-01 19:00:57 +0300

    add routes

--- server/routes/postRouter.js
+++ server/routes/postRouter.js
@@ -8,7 +8,7 @@ postRouter.get('/subs/channels/:offset', async (req, res) => {
   const userId = req.session.user?.id;
 
   if (!userId) {
-    return res.status(401).json({ message: 'Unathorized' });
+    return res.status(401).json({ message: 'Unauthorized' });
   }
 
   const channels = await Subscription.findAndCountAll({
@@ -21,6 +21,29 @@ postRouter.get('/subs/channels/:offset', async (req, res) => {
   });
 
   return res.json({ ...channels, rows: channels.rows.map((el) => el.Channel) });
+});
+
+postRouter.get('/subs', async (req, res) => {
+  const userId = req.session.user?.id;
+
+  console.log(userId);
+
+  if (!userId) {
+    return res.status(401).json({ message: 'Unauthorized' });
+  }
+
+  const videos = await Subscription.findAll({
+    where: { userId },
+    include: {
+      model: Channel,
+      include: {
+        model: Video,
+        include: Channel
+      },
+    },
+  });
+
+  return res.json(videos.map((el) => el.Channel.Videos).flat());
 });
 
 postRouter.get('/random', async (req, res) => {
