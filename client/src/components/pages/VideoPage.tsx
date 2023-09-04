7c26c6652bc0a1c7bde11093344c1975dbddecf3
monikopp <mmonikopp@gmail.com>
2023-09-04 10:33:12 +0300

    коты на 100 проц

--- client/src/components/pages/VideoPage.tsx
+++ client/src/components/pages/VideoPage.tsx
@@ -17,66 +17,72 @@ import { useParams } from 'react-router-dom';
 import MenuLeft from '../ui/MenuLeft';
 import NavBar from '../ui/NavBar';
 
-export default function VideoPage():JSX.Element {
+export default function VideoPage(): JSX.Element {
   const { link } = useParams();
   return (
-    <div >
- <MenuLeft />
- <NavBar />
- <Stack style={{marginTop: 90, display:'flex', alignContent: 'center', zIndex:-3}} spacing={1} >
- <div >
- <Card style={{marginTop: 0}}>
-      <CardContent style={{display:'flex', flexDirection:'column', alignItems: 'start'}}>
-      {/* <Skeleton variant="rounded" width={910} height={500} style={{alignSelf: 'center'}}/> */}
-      <video id="videoPlayer"  style={{alignSelf: 'center'}} 
-      // width="650px" 
-      width={910} height={500}
-      controls muted="muted" autoPlay>
-        {link && <source src={`http://localhost:3001/api/watch/${link}`} />}
-      </video>
-       <Typography color="text.secondary">
-          54623754 просмотров | опубликовано когда-то
-        </Typography>
-        <Divider variant="inset" />
-        <div style={{display:'flex', flexDirection:'row'}} >
-        <ListItem>
-        <ListItemAvatar>
-          <Avatar alt="Remy Sharp" src="" />
-        </ListItemAvatar>
-        <ListItemText>
-              <Typography>
-                Ali Connors
-              </Typography>
-              <Typography color="text.secondary">
-          54623754 subscribers
-        </Typography>
-            </ListItemText>
-              <Button style={{marginRight: '-147%'}} variant="contained">Подписаться</Button>
-      </ListItem>
-      </div>
-      <Divider variant="inset" />
+    <div>
+      <MenuLeft />
+      <NavBar />
+      <Stack
+        style={{ marginTop: 90, display: 'flex', alignContent: 'center', zIndex: -3 }}
+        spacing={1}
+      >
         <div>
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-          >
-          <Typography>Read Description</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-    </div>
-      </CardContent>
-    </Card>
-    </div>
-          </Stack>
-    </div>
-  )
-}
+          <Card style={{ marginTop: 0 }}>
+            <CardContent style={{ display: 'flex', flexDirection: 'column', alignItems: 'start' }}>
+              {/* <Skeleton variant="rounded" width={910} height={500} style={{alignSelf: 'center'}}/> */}
+              <video
+                id="videoPlayer"
+                style={{ alignSelf: 'center' }}
+                width={910}
+                height={500}
+                controls
+                muted="muted"
+                autoPlay
+              >
+                {link && <source src={`http://localhost:3001/api/watch/${link}`} />}
+              </video>
 
+              <Typography color="text.secondary">
+                54623754 просмотров | опубликовано когда-то
+              </Typography>
+              <Divider variant="inset" />
+              <div style={{ display: 'flex', flexDirection: 'row' }}>
+                <ListItem>
+                  <ListItemAvatar>
+                    <Avatar alt="Remy Sharp" src="" />
+                  </ListItemAvatar>
+                  <ListItemText>
+                    <Typography>Ali Connors</Typography>
+                    <Typography color="text.secondary">54623754 subscribers</Typography>
+                  </ListItemText>
+                  <Button style={{ marginRight: '-147%' }} variant="contained">
+                    Подписаться
+                  </Button>
+                </ListItem>
+              </div>
+              <Divider variant="inset" />
+              <div>
+                <Accordion>
+                  <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
+                  >
+                    <Typography>Read Description</Typography>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    <Typography>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada
+                      lacus ex, sit amet blandit leo lobortis eget.
+                    </Typography>
+                  </AccordionDetails>
+                </Accordion>
+              </div>
+            </CardContent>
+          </Card>
+        </div>
+      </Stack>
+    </div>
+  );
+}
