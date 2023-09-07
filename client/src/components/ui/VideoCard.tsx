8d79ba4551b85dd43febd3644eb39c01a349c680
monikopp <mmonikopp@gmail.com>
2023-09-07 13:53:39 +0300

    fix

--- client/src/components/ui/VideoCard.tsx
+++ client/src/components/ui/VideoCard.tsx
@@ -41,8 +41,8 @@ export default function VideoCard({ video }: VideoCardProps): JSX.Element {
         />
         <Box sx={{ display: 'flex', flexDirection: 'column', flexWrap: 'wrap' }}>
           <CardContent sx={{ flex: '1 0 auto' }}>
-            <Typography component="div" variant="h6">
-              {video.title}
+            <Typography component="div" variant="h6" style={{display:'flex',flexWrap:'wrap',overflow:"hidden"}}>
+              {video.title.length > 23 ? video.title.substring(0,23) + '...' : video.title}
             </Typography>
             <Typography variant="subtitle1" color="text.secondary" component="div">
               <Stack direction="row" spacing={2} style={{ padding: '1px', alignItems: 'center' }}>
