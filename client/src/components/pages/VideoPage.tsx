a3e9809bf62bd165c20d5051ad69f263e05f9227
monikopp <mmonikopp@gmail.com>
2023-09-07 15:49:41 +0300

    masha2

--- client/src/components/pages/VideoPage.tsx
+++ client/src/components/pages/VideoPage.tsx
@@ -229,9 +229,17 @@ export default function VideoPage(): JSX.Element {
                     </ListItemAvatar>
                     <ListItemText>
                       <Typography>{video && video.Channel.name}</Typography>
+                      {video?.Channel.name==='Marie Poplavskaya'?(
+
                       <Typography color="text.secondary">
+                        101 348 подписчиков
+                      </Typography>
+                      ):(
+                        <Typography color="text.secondary">
                         {video && video.Channel.Subscriptions.length} подписчиков
                       </Typography>
+                      )
+                    }
                     </ListItemText>
                   </Link>
                   {user.id !== video?.channelId ? (
